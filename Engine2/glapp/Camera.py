1bfdebd1e69f2fc5a130f8f406aa38cc5028a5ba
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2022-07-04 17:24:34 -0400

    all the code I have for this engine

--- Engine2/glapp/Camera.py
+++ Engine2/glapp/Camera.py
@@ -1,53 +1,59 @@
 import pygame
 from OpenGL.GLU import *
 from math import *
+import numpy as np
+from .Transformations import *
 
 class Camera:
-    def __init__(self):
-        self.eye = pygame.math.Vector3(1, 1, 3)
-        self.up = pygame.math.Vector3(0, 1, 0)
-        self.right = pygame.math.Vector3(1, 0, 0)
-        self.forward = pygame.math.Vector3(0, 0, 1)
-        self.look = self.eye + self.forward
-
-        self.yaw = -105
-        self.pitch = -5
+    def __init__(self, program_id, w, h):
+        self.transformation = identity_mat()
         self.last_mouse = pygame.math.Vector2(0, 0)
         self.mouse_sensitivityX = 0.1
         self.mouse_sensitivityY = 0.1
         self.key_sensitivity = 0.001
+        self.projection_mat = self.perspective_mat(60, w/h, 0.01, 10000)
+
+    def perspective_mat(self, angle_of_view, aspect_ratio, near_plane, far_plane):
+        view_angle = radians(angle_of_view)
+        d = 1.0 / tan(view_angle/2)
+        r = aspect_ratio
+        b = (far_plane + near_plane) / (near_plane - far_plane)
+        c = (far_plane * near_plane) / (near_plane - far_plane)
+        return np.array([d/r, 0, 0, 0],
+                        [0, d, 0, 0],
+                        [0, 0, b, c],
+                        [0, 0, -1, 0], np.float32)
 
     def rotate(self, yaw, pitch):
-        self.yaw += yaw
-        self.pitch += pitch
-        self.forward.x = cos(radians(self.yaw)) * cos(radians(self.pitch))
-        self.forward.y = sin(radians(self.pitch))
-        self.forward.z = sin(radians(self.yaw)) * cos(radians(self.pitch))
-        self.foward = self.forward.normalize()
-        self.right = self.foward.cross(pygame.Vector3(0, 1, 0)).normalize()
-        self.up = self.right.cross(self.foward).normalize()
+        # self.yaw += yaw
+        # self.pitch += pitch
+        # self.forward.x = cos(radians(self.yaw)) * cos(radians(self.pitch))
+        # self.forward.y = sin(radians(self.pitch))
+        # self.forward.z = sin(radians(self.yaw)) * cos(radians(self.pitch))
+        # self.foward = self.forward.normalize()
+        # self.right = self.foward.cross(pygame.Vector3(0, 1, 0)).normalize()
+        # self.up = self.right.cross(self.foward).normalize()
+
+        self.transformation = rotate(self.transformation, yaw, 'y')
+        self.transformation = rotate(self.transoformation, pitch, 'x')
 
     def update(self, w, h):
         mouse_pos = pygame.mouse.get_pos()
         mouse_change = self.last_mouse - pygame.math.Vector2(mouse_pos)
         pygame.mouse.set_pos(w/2, h/2)
         self.last_mouse = pygame.mouse.get_pos()
-        self.rotate(-mouse_change.x * self.mouse_sensitivityX, mouse_change.y * self.mouse_sensitivityY)
-        if self.pitch > 89.0:
-            self.pitch = 89.0
-        if self.pitch < -89.0:
-            self.pitch = -89.0
+        self.rotate(mouse_change.x * self.mouse_sensitivityX, mouse_change.y * self.mouse_sensitivityY)
+
+        # if self.pitch > 89.0:
+        #     self.pitch = 89.0
+        # if self.pitch < -89.0:
+        #     self.pitch = -89.0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_s]:
-            self.eye -= self.forward * self.key_sensitivity
+            self.transformation = translate(self.transformation, 0, 0, self.key_sensitivity)
         if keys[pygame.K_w]:
-            self.eye += self.forward * self.key_sensitivity
+            self.transformation = translate(self.transformation, 0, 0, -self.key_sensitivity)
         if keys[pygame.K_a]:
-            self.eye -= self.right * self.key_sensitivity
+            self.transformation = translate(self.transformation, self.key_sensitivity, 0, 0)
         if keys[pygame.K_d]:
-            self.eye += self.right * self.key_sensitivity
-
-        self.look = self.eye + self.forward
-        gluLookAt(self.eye.x, self.eye.y, self.eye.z,
-                  self.look.x, self.look.y, self.look.z,
-                  self.up.x, self.up.y, self.up.z)
+            self.transformation = translate(self.transformation, -self.key_sensitivity, 0, 0)
--- Engine2/shader.py
+++ Engine2/shader.py
@@ -2,15 +2,18 @@ from glapp.PyOGLApp import *
 import numpy as np
 from glapp.Utils import *
 from glapp.GraphicsData import *
+from glapp.Square import *
 
 vertex_shader = r'''
 #version 330 core
 in vec3 pos;
 in vec3 vertex_color;
+uniform vec3 translation;
 out vec3 color;
 void main() 
 {
-    gl_Position = vec4(pos.x, pos.y, pos.z, 1);
+    vec3 pos = pos + translation;
+    gl_Position = vec4(pos, 1);
     color = vertex_color;
 }
 '''
@@ -28,26 +31,11 @@ void main()
 class FirstShader(PyOGLApp):
     def __init__(self):
         super().__init__(850, 200, 1000, 800)
-        self.vao_ref = None
-        self.vertex_count = 0
+        self.square = None
 
     def initialize(self):
         self.program_id = create_program(vertex_shader, fragment_shader)
-        self.vao_ref = glGenVertexArrays(1)
-        glBindVertexArray(self.vao_ref)
-        glPointSize(10)
-        position_data = [[0, -0.9, 0],
-                         [-0.6, 0.8, 0],
-                         [0.9, -0.2, 0],
-                         [-0.9, -0.2, 0],
-                         [0.6, 0.8, 0]]
-        self.vertex_count = len(position_data)
-        position_variable = GraphicsData("vec3", position_data)
-        position_variable.create_variable(self.program_id, "pos")
-        color_data = [[1, 0 ,0], [0, 1, 0], [0, 0, 1], [1, 0, 1], [1, 1, 0]]
-        color_var = GraphicsData("vec3", color_data)
-        color_var.create_variable(self.program_id, "vertex_color")
-
+        self.square = Square(self.program_id, pygame.Vector3(-1,1,0))
 
     def camera_init(self):
         pass
@@ -55,6 +43,6 @@ class FirstShader(PyOGLApp):
     def display(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glUseProgram(self.program_id)
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, self.vertex_count)
+        self.square.draw()
 
 FirstShader().mainloop()
